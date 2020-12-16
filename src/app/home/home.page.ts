ee6e495b7a7a17817b91ac863c776b3da35094ec
Invitado Ocirio <xav.xgca@gmail.com>
2020-12-16 17:38:06 -0500

    Chat con ionic y angular

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,81 @@
+// home.page.ts
+import { Component } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+interface StudentData {
+  Name: string;
+  Address: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  studentList = [];
+  studentData: StudentData;
+  studentForm: FormGroup;
+
+  constructor(
+    private firebaseService: FirebaseService,
+    public fb: FormBuilder
+  ) {
+    this.studentData = {} as StudentData;
+  }
+
+  ngOnInit() {
+
+    this.studentForm = this.fb.group({
+      Name: ['', [Validators.required]],
+      Mensaje: ['', [Validators.required]]
+    })
+
+    this.firebaseService.read_students().subscribe(data => {
+
+      this.studentList = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          Name: e.payload.doc.data()['Name'],
+          Mensaje: e.payload.doc.data()['Mensaje'],
+        };
+      })
+
+    });
+  }
+
+  CreateRecord() {
+    this.firebaseService.create_student(this.studentForm.value)
+      .then(resp => {
+        //Reset form
+        this.studentForm.reset();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  RemoveRecord(rowID) {
+    this.firebaseService.delete_student(rowID);
+  }
+
+  EditRecord(record) {
+    record.isEdit = true;
+    record.EditName = record.Name;
+    record.EditAge = record.Age;
+    record.EditAddress = record.Address;
+  }
+
+  UpdateRecord(recordRow) {
+    let record = {};
+    record['Name'] = recordRow.EditName;
+    record['Age'] = recordRow.EditAge;
+    record['Address'] = recordRow.EditAddress;
+    this.firebaseService.update_student(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
+
+}
